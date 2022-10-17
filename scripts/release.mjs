b3bc97dedb3e86b0af13c2911fe72b067edd420d
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2022-10-17 20:42:11 +0300

    1.1.0.3

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -50,6 +50,8 @@ execSync(`npx prettier --write ${MANIFEST}`);
 console.log('\nGenerating release:', json.version);
 execSync('git add .');
 execSync(`git commit -m "${json.version}"`);
+const rv = execSync('git status --porcelain');
+console.log(rv);
 execSync(`git tag -a ${json.version} -m "${json.version}"`);
 // execSync(`git tag -f latest`);
 console.log('Pushing release');
