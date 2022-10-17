bde761835dfe1152895cc4d0b77b55a8a4552c65
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2022-10-17 19:13:52 +0300

    Dev env update

--- scripts/esbuild.mjs
+++ scripts/esbuild.mjs
@@ -8,23 +8,24 @@ const OUT_DIR = './dist';
 
 const packData = fs.readFileSync('./package.json');
 const packJSON = JSON.parse(packData);
-const mainFile = packJSON.main;
+const mainFile = packJSON.source;
 
 const args = process.argv.slice(2);
-
 const watch = args.includes('--watch');
 
 async function build() {
 	const res = await esbuild.build({
 		entryPoints: [mainFile],
 		bundle: true,
-		outfile: path.join(OUT_DIR, mainFile),
+		// absWorkingDir: path.resolve(OUT_DIR),
+		outfile: path.join(OUT_DIR, path.parse(mainFile).base),
+		// outdir: path.resolve(OUT_DIR),
 		metafile: true,
 		sourcemap: true,
-		minify: true,
-		minifyIdentifiers: true,
-		minifyWhitespace: true,
-		minifySyntax: true,
+		minify: !watch,
+		minifyIdentifiers: false,
+		minifyWhitespace: !watch,
+		minifySyntax: !watch,
 		keepNames: true,
 		platform: 'browser',
 		format: 'esm',
@@ -62,4 +63,14 @@ async function build() {
 	}
 }
 
-build();
+await build();
+
+// Fix for esbuild bug: https://github.com/evanw/esbuild/issues/2401
+if (!watch) {
+	const sourceFile = JSON.parse(fs.readFileSync('./package.json')).source;
+	const mapFile = `./dist/${sourceFile}.map`;
+	const mapData = fs.readFileSync(mapFile, { encoding: 'utf-8' });
+	const json = JSON.parse(mapData);
+	json.sources = json.sources.map(s => s.replace(/^\.\.\//, '')); // Remove initial ../ caused by dist folder use
+	fs.writeFileSync(mapFile, JSON.stringify(json), { encoding: 'utf-8' });
+}
